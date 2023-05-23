59f4f1a6046f5cf8b757646217f6c3c136d49906
jaehyung ryu <jaehyungryu@jaehyungui-MacBookPro.local>
2023-05-23 13:32:40 +0900

    main modal done

---
+++ src/view/router/service/Service.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Service(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Service;
--- src/view/template/header/Header.js
+++ src/view/template/header/Header.js
@@ -1,38 +1,107 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style/Header.scss';
-import logo from "../../asset/images/logo.jpeg";
+import Modal from 'react-modal';
 
 function Header(props) {
-    return (
-        <div id={"Header"}>
-            <div className="header-container">
-                <div className="left-wrapper">
-                    <div className="logo-wrapper">
-                        {/*<img alt='' src={logo} className='logo-img'/>*/}
-                        KHU
-                    </div>
-                    <div className="tab-wrapper">
-                        <div className="tab-box-wrapper">
-                            Overview
-                        </div>
-                        <div className="tab-box-wrapper">
-                            Technique
-                        </div>
-                        <div className="tab-box-wrapper">
-                            Review
-                        </div>
-                        <div className="tab-box-wrapper">
-                            Contact
-                        </div>
 
+    const customStyles = {
+        content: {
+            position:'absolute',
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+
+    const [isLogInModalOpen, setIsLogInModalOpen] = useState(false);
+
+    const [isLogIn, setIsLogIn] = useState(false);
+
+    const [password, setPassword] = useState('');
+
+    const onChangePw = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const onClickConfirm = () =>{
+        if(password === '1234'){
+            setIsLogIn(true);
+            setIsLogInModalOpen(false);
+        }
+        else{
+            window.alert('Wrong password!');
+        }
+    }
+
+    console.log(password);
+
+    const onClickLogIn = () => {
+        setIsLogInModalOpen(true);
+    }
+
+    return (
+        <>
+            <Modal
+                className='log-in-modal'
+                isOpen={isLogInModalOpen}
+                onRequestClose={() => setIsLogInModalOpen(false)}
+                style={customStyles}
+                ariaHideApp={false}
+            >
+                <div className="modal-title">
+                    Log In
+                </div>
+                <div className="modal-content">
+                    <div className="input-wrapper">
+                        <div className="input-text">Enter Admin Password</div>
+                        <input type="password" className="pw-input" onChange={(e)=>onChangePw(e)}/>
+                    </div>
+                    <div className="btn-wrapper">
+                        <div className="btn close" onClick={()=>setIsLogInModalOpen(false)}>Close</div>
+                        <div className="btn confirm" onClick={()=>onClickConfirm()}>OK</div>
                     </div>
                 </div>
-                <div className="btn-wrapper">
-                    <div className="btn sign-up">회원가입</div>
-                    <div className="btn sign-in">로그인</div>
+
+
+            </Modal>
+            <div id={"Header"}>
+                <div className="header-container">
+                    <div className="left-wrapper">
+                        <div className="logo-wrapper">
+                            KHU
+                        </div>
+                        <div className="tab-wrapper">
+                            <div className="tab-box-wrapper">
+                                Overview
+                            </div>
+                            <div className="tab-box-wrapper">
+                                Technique
+                            </div>
+                            <div className="tab-box-wrapper">
+                                Review
+                            </div>
+                            <div className="tab-box-wrapper">
+                                Contact
+                            </div>
+
+                        </div>
+                    </div>
+                    {
+                        isLogIn ?
+                            <div className="btn-wrapper">
+                                <div className="btn sign-in">Start</div>
+                            </div>
+                            :
+                            <div className="btn-wrapper" onClick={() => onClickLogIn()}>
+                                <div className="btn sign-in">Log In</div>
+                            </div>
+                    }
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
