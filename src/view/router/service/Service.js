d95408b3e1277a0673f7d4be701979427fa6582c
hyungjun KIM <hyungjunkim@hyungjunui-MacBookPro.local>
2023-05-23 14:58:58 +0900

    axios

--- src/view/router/service/Service.js
+++ src/view/router/service/Service.js
@@ -1,46 +1,70 @@
-import React, {useState} from 'react';
-import './style/Service.scss';
+import React, { useState } from "react";
+import axios from "axios";
+import "./style/Service.scss";
 
 function Service(props) {
+  const [inputText, setInputText] = useState("");
+  const [responseText, setResponseText] = useState([]);
+  const [textValue, setTextValue] = useState("");
+  const [chatList, setChatList] = useState([]);
 
-    const [textValue, setTextValue] = useState('');
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
 
-    const [chatList, setChatList] = useState([]);
+  const handleButtonClick = () => {
+    axios
+      .post("http://127.0.0.1:5000/predict", { text: textValue })
+      .then((response) => {
+        setResponseText((prevResponseText) => [
+          ...prevResponseText,
+          response.data.result,
+        ]);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-    const onTypeEnter = (e) => {
-        e.preventDefault();
-        if (e.keyCode === 13) {
-            //chatList에 추가, 오른쪽에 표시(내 채팅)
-            chatList.push(textValue);
 
-            //등록 후 초기화
-            setTextValue('');
-        }
+  const onTypeEnter = (e) => {
+    e.preventDefault();
+    if (e.keyCode === 13) {
+      //chatList에 추가, 오른쪽에 표시(내 채팅)
+      chatList.push(textValue);
+
+      //등록 후 초기화
+      setTextValue("");
     }
+  };
 
-    console.log(chatList);
+  console.log(chatList);
 
-    return (
-        <div id={"Service"}>
-            {/*<div className="description-wrapper"></div>*/}
-            <div className="chat-wrapper">
-                {
-                    chatList.map((li, index) => {
-                        return (
-                            <div className="chat-row-wrapper" key={index}>
-                                {chatList[index]}
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <div id={"Service"}>
+      {/*<div className="description-wrapper"></div>*/}
+      <div className="chat-wrapper">
+        {chatList.map((response, index) => {
+          return (
+            <div className="chat-row-wrapper" key={index}>
+                  {chatList[index]}
+                  {response}
             </div>
-            <div className="chat-input-wrapper">
-                <input type="text" className="chat-input" value={textValue}
-                       onChange={(e) => setTextValue(e.target.value)} onKeyUp={(e) => onTypeEnter(e)}/>
-                <div className="enter-btn">Enter</div>
-            </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+      <div className="chat-input-wrapper">
+        <input
+          type="text"
+          className="chat-input"
+          value={textValue}
+          onChange={(e) => setTextValue(e.target.value)}
+          onKeyUp={(e) => onTypeEnter(e)}
+        />
+        <div className="enter-btn">Enter</div>
+      </div>
+    </div>
+  );
 }
 
 export default Service;
