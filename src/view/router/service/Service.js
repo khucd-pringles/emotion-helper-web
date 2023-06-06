c35f0b10ec0cc6fbae73d3ba1981c8aa30d67237
JeongEuiwang <euiwang0323@gmail.com>
2023-06-06 17:09:14 +0900

    fix service ui

--- src/view/router/service/Service.js
+++ src/view/router/service/Service.js
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import "./style/Service.scss";
-
+import UserAvatar from '../../asset/images/image_user_avatar.png'
 function Service(props) {
 
   const [responseData, setResponseData] = useState([]);
   const [textValue, setTextValue] = useState("");
-  const [chatList, setChatList] = useState([]);
+  const [chatList, setChatList] = useState([
+    '안녕!', {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 계시네요!'},
+    '안녕!안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 계시네요!행복을 느끼고 계시네요!행복을 느끼고 계시네요!행복을 느끼고 계시네요!행복을 느끼고 계시네요!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 asdsadsad계시네요!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 asd계시네요!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 !'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복네요!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복asdasassaddadadasd을 느끼고 계시네요!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 계시네요!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 계asdasdasdaddsdasdsdasdasdasa시네요!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행!'}, '안녕!',
+    {feel_list: ['행복', '슬픔', '웃음'], result: '행복을 느끼고 계시네요!'}, '안녕!',
+
+  ]);
 
   const handleButtonClick = () => {
     axios
-      .post("http://127.0.0.1:5000/predict", { text: textValue } )
+      .post("http://127.0.0.1:5000/predict", {text: textValue})
       .then((response) => {
         console.log(response);
         setResponseData((prevResponseData) => [
-            ...prevResponseData,
-            response.data
+          ...prevResponseData,
+          response.data
         ])
         chatList.push(response.data);
       })
@@ -35,7 +49,7 @@ function Service(props) {
     }
   };
 
-  console.log('chatList',chatList)
+  console.log('chatList', chatList)
 
   return (
     <div id={"Service"}>
@@ -43,24 +57,26 @@ function Service(props) {
       <div className="chat-wrapper">
         {chatList.map((li, index) => {
           return (
-              <div key={index}>
-                  {index % 2 === 0 ?
-                      <div className={"user-chat-row-wrapper"} key={index}>
-                          <div className="user-chat-box">
-                              {li}
-                          </div>
-                      </div>
-                  :
-                      <>
-                          <div className="emotion-wrapper">Bert가 분석한 User 감정 : {li.feel_list[0]}</div>
-                          <div className={"model-chat-row-wrapper"} key={index}>
-                              <div className="model-chat-box">
-                                  {li.result}
-                              </div>
-                          </div>
-                      </>
-                  }
-              </div>
+            <div key={index}>
+              {index % 2 === 0 ?
+                <>
+                  <div className={"user-chat-row-wrapper"} key={index}>
+                    <div className="user-chat-box">
+                      {li}
+                    </div>
+                  </div>
+                </>
+                :
+                <>
+                  <div className="emotion-wrapper">Bert가 분석한 당신의 감정 : <span style={{fontWeight: 500, color: '#7950f7'}}>{li.feel_list[0]}</span></div>
+                  <div className={"model-chat-row-wrapper"} key={index}>
+                    <div className="model-chat-box">
+                      {li.result}
+                    </div>
+                  </div>
+                </>
+              }
+            </div>
           );
         })}
       </div>
@@ -68,11 +84,12 @@ function Service(props) {
         <input
           type="text"
           className="chat-input"
+          placeholder={'Bert에게 채팅을 전송해 보세요'}
           value={textValue}
           onChange={(e) => setTextValue(e.target.value)}
           onKeyUp={(e) => onTypeEnter(e)}
         />
-        <div className="enter-btn" onClick={()=>handleButtonClick()}>Enter</div>
+        <div className="enter-btn" onClick={() => handleButtonClick()}>Enter</div>
       </div>
     </div>
   );
